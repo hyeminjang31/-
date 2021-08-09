cd90f67241dd43b44be41f4a2edab6feea986e57
hyeminjang31 <86878688+hyeminjang31@users.noreply.github.com>
2021-08-09 13:56:19 +0900

    java day01

---
+++ week1.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+
+public class week1 {
+
+	public static void main(String[] args) {
+		System.out.println("Enter two whole nnumbers on a line");
+		int n1,n2;
+		Scanner keyboard=new Scanner(System.in);
+		n1=keyboard.nextInt();
+		n2=keyboard.nextInt();
+		System.out.println("The sum of those two number is ");
+		System.out.println(n1+n2);
+		
+
+	}
+
+}
